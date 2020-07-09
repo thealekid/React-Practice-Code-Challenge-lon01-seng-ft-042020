63224e7c9c2dbf993ab261020183aeffb02bab45
thealekid <chukorakwusi@gmail.com>
2020-07-09 13:59:47 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,64 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sushis: [],
+      total_remaining: 1000,
+      startingSushi: 0,
+      usedPlates: []
+    };
+  }
 
+  componentDidMount() {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          sushis: data,
+        })
+      );
+  }
+
+
+  render4Sushis = () => {
+    return this.state.sushis.slice(this.state.startingSushi, this.state.startingSushi + 4)
+  }
+
+  incrementSushi = () => {
+    this.setState ({
+      startingSushi: this.state.startingSushi + 4
+    })
+  }
+
+  decrementPrice = (price, id) => {
+    let emptyPlates = [...this.state.usedPlates]
+    emptyPlates.push(id)
+    console.log(emptyPlates)
+    this.setState ({
+      total_remaining: this.state.total_remaining - price,
+      usedPlates: emptyPlates
+    })
+  }
+  
+  
+  
   render() {
+    // console.log(this.state.sushis.slice(0, 4));
+    console.log(this.state)
+    const sushisToRender = this.render4Sushis()
+  
+
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={sushisToRender} more4={this.incrementSushi} decrementPrice={this.decrementPrice} usedPlates={this.state.usedPlates}/>
+        <Table money={this.state.total_remaining} usedPlates={this.state.usedPlates}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  
+  const renderSushis = () => {
+  return props.sushis.map(sushi => { 
+      const isEaten = props.usedPlates.includes(sushi.id)
+    return <Sushi id={sushi.id} key={sushi.id} name={sushi.name} price={sushi.price} image_url={sushi.img_url} decrementPrice={props.decrementPrice} isEaten={isEaten}/> })
+  }
+
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {renderSushis()}
+        <MoreButton addSushiBtn={props.more4}/>
       </div>
     </Fragment>
   )
